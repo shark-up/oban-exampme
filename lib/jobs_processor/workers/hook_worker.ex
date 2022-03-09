1184036a69c275520335c8d48d94db2d9364aab6
jeremieflandrin002 <jeremie.flandrin@gmail.com>
2022-03-09 15:36:30 +0100

    Example

--- lib/jobs_processor/workers/hook_worker.ex
+++ lib/jobs_processor/workers/hook_worker.ex
@@ -5,14 +5,25 @@ defmodule JobsProcessor.HookWorker do
     queue: :hooks,
     priority: 3,
     max_attempts: 5,
-    tags: ["hook"],
-    unique: [period: 30]
+    tags: ["hook"]
+
+  @delay 3 * 1000
 
   @impl Oban.Worker
-  def perform(job) do
-    IO.inspect(job)
-    Logger.warn("run job")
+  def perform(args) do
+    Logger.warn("start to perform")
 
-    :ok
+    IO.inspect(args: args)
+    performing()
+
+    Logger.warn("done")
+
+    # If returns :ok, then the job is done for Oban
+    # elise if {:error, ...} then retry in n time (4 strategies for the backoff)
+    Enum.random([:ok, {:error, "An error ..."}])
+  end
+
+  def performing do
+    :timer.sleep(@delay)
   end
 end
